fef8b47335e340171571960d4ffb1202aa84b80d
sean-cho <soonwengcho@gmail.com>
2016-12-21 09:29:51 -0500

    Bug fixes.

--- setup_master.sh
+++ setup_master.sh
@@ -56,8 +56,9 @@ do
 done
 
 # Increase user limits for root for current session in master node #
-ulimit -Hn 10000
-ulimit -Sn 5000
+# ulimit -Hn 10000
+# ulimit -Sn 5000
+# Not necessary
 
 # Configure to increase the soft and hard limits for opened files in master node #
 
